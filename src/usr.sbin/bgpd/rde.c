6bc939abc5b95bef200e59c7b1ad68456ae4b75c
Claudio Jeker <claudio@openbsd.org>
2026-05-18 11:36:34 +0200

    The pt_loookup path in the Adj-RIB-Out case may miss entries if the more specific prefix was filtered but Adj-RIB-Out holds a prefix that is larger.
    Therefor this needs yet another loop. Gulp.

--- src/usr.sbin/bgpd/rde.c
+++ src/usr.sbin/bgpd/rde.c
@@ -3382,14 +3382,16 @@ rde_dump_ctx_new(struct ctl_show_rib_request *req, pid_t pid,
 						    pte, p, ctx);
 						found = 1;
 					}
+					plen = pte->prefixlen - 1;
+					pte = NULL;
 					if (!found &&
 					    req->prefixlen == hostplen) {
-						for (plen = pte->prefixlen - 1;
-						    plen >= 0; plen--) {
+						while (plen >= 0) {
 							pte = pt_get(
 							    &req->prefix, plen);
 							if (pte != NULL)
 								break;
+							plen--;
 						}
 					}
 				} while (!found && pte != NULL);
